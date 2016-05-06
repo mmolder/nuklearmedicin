961c8f2ed2f0f6abbcaf9a60a7351840df3b1d76
Mikael Mölder <Mikael Mlder>
2016-05-06 16:07:37 +0200

    minor adds

--- Nuklearmedicin/app/src/main/java/com/nuklearmedicin/fragments/AboutFragment.java
+++ Nuklearmedicin/app/src/main/java/com/nuklearmedicin/fragments/AboutFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.nuklearmedicin.HandleXml;
 import com.nuklearmedicin.R;
@@ -16,7 +17,8 @@ import com.nuklearmedicin.R;
  */
 public class AboutFragment extends Fragment {
 
-    WebView wv;
+    TextView title;
+    TextView undertitle;
 
     @Nullable
     @Override
@@ -24,14 +26,14 @@ public class AboutFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
-        wv = (WebView) rootView.findViewById(R.id.about_webview);
+        title = (TextView) rootView.findViewById(R.id.about_title);
+        undertitle = (TextView) rootView.findViewById(R.id.about_undertitle);
 
         HandleXml handleXml = new HandleXml();
         String restarray[] = handleXml.getContent(getContext(), 1, "About");
 
-        String justified = "<html><body style=\"text-align:justify\">" + restarray[0] + "</body></html>";
-
-        wv.loadData(justified, "text/html; charset=utf-8", "utf-8");
+        title.setText("Om applikationen");
+        undertitle.setText(restarray[0].replace("\\n","\n"));
 
         return rootView;
 
